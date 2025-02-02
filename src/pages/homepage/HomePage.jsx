05d422ed67dbfe1a2ebbd18c52a7d8199f3a06e0
mahmoud makhaly <mahmoud.abdelfatah@5d-agency.com>
2025-02-02 10:24:07 +0200

    -add z-index to navbar

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -114,36 +114,6 @@ const HomePage = () => {
       <div className=" position-fixed shape-container">
         <img src={bgImage} alt="" />
       </div>
-    
-      {/* <Banner
-        tabs={[
-          {
-            tabName: "Capture",
-            tabContent: "Content 1",
-            id: 0,
-          },
-          {
-            tabName: "Animate",
-            tabContent: "Content 2",
-            id: 1,
-          },
-          {
-            tabName: "Live stream",
-            tabContent: "Content 3",
-            id: 2,
-          },
-          {
-            tabName: "Edit",
-            tabContent: "Content 4",
-            id: 3,
-          },
-          {
-            tabName: "Export",
-            tabContent: "Content 5",
-            id: 4,
-          },
-        ]}
-      /> */}
     </div>
   );
 };
