c2d62ff67c8887d81b76bb3440c3b63843a0540d
mahmoud makhaly <mahmoud.abdelfatah@5d-agency.com>
2025-02-04 16:59:46 +0200

    -create mocap suits sections

--- src/pages/homepage/mocap-suits/MocapSuits.jsx
+++ src/pages/homepage/mocap-suits/MocapSuits.jsx
@@ -1,215 +1,74 @@
+import { Col, Container, Row } from "reactstrap";
+import { Button } from "@components/common";
 const MocapSuits = () => {
   return (
-    <section className="section padding-xx-huge t_padding-huge m_padding-x-large bg_section">
-      <div className="container">
-        <div className="display-flex direction-vertical gap-x-large">
-          <div className="display-flex direction-vertical align-center">
-            <h2>Mocap suits vs video-based mocap systems</h2>
+    <div className="py-4 bg_section MocapSuits">
+      <section className="py-5  ">
+        <Container className="py-5">
+          <h2 className="text-center mb-3">
+            Mocap suits vs video-based mocap systems
+          </h2>
+          <Row className="py-4   ">
+            <Col md={4}></Col>
+            <Col md={4}>
+              <h5 className="text-gray title">Mocap suits</h5>
+            </Col>
+            <Col md={4}>
+              <h5 className="text-gray title">Video-based mocap systems</h5>
+            </Col>
+          </Row>{" "}
+          <Row className="py-4  ">
+            <Col md={4}>
+              <h5 className="text-gray d-flex gap-2">
+                Real-time tracking
+                <span className="info">i</span>
+              </h5>
+            </Col>
+            <Col md={4}>
+              <h5>Supported</h5>
+            </Col>
+            <Col md={4}>
+              <h5>Not supported</h5>
+            </Col>
+          </Row>
+          <Row className="py-4  ">
+            <Col md={4}>
+              {" "}
+              <h5 className="text-gray d-flex gap-2">
+                Tracking space <span className="info">i</span>
+              </h5>
+            </Col>
+            <Col md={4}>
+              {" "}
+              <h5>Flexible, mocap anywhere</h5>
+            </Col>
+            <Col md={4}>
+              {" "}
+              <hf>Limited, distance constraints</hf>
+            </Col>
+          </Row>
+          <Row className="py-4  ">
+            <Col md={4}>
+              {" "}
+              <h5 className="text-gray d-flex gap-2">
+                Set up time <span className="info">i</span>
+              </h5>
+            </Col>
+            <Col md={4}>
+              {" "}
+              <h5>Fast, 1 min</h5>
+            </Col>
+            <Col md={4}>
+              {" "}
+              <h5>Slow, 15 min</h5>
+            </Col>
+          </Row>
+          <div className="display-flex justify-center mt-4">
+            <Button variant="outline"> See the full comparison</Button>
           </div>
-          <div className="position-relative m_overflow-auto no-scrollbar">
-            <div className="position-relative">
-              <div className="padding-top padding-xx-large"></div>
-              <div className="padding-top padding-tiny"></div>
-              <div className="padding-top padding-micro t_display-none"></div>
-              <div className="position-absolute top-0 right-0 bottom-0 left-0 pointer-events-none">
-                <div className="secondary-nav bg-dark-900 pointer-events-auto m_top-0">
-                  <div className="display-flex">
-                    <div className="width-33pct sizing-fixed border-bottom border-color-grey-500 m_width-45pct"></div>
-                    <div className="width-33pct sizing-fixed border-bottom border-color-grey-500 m_width-full">
-                      <div className="padding-vertical padding-small">
-                        <div className="padding-right padding-small">
-                          <h3 className="h _5 color-grey-500">Mocap suits</h3>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="width-33pct sizing-fixed border-bottom border-color-grey-500 m_width-full">
-                      <div className="padding-vertical padding-small">
-                        <div className="padding-right padding-small">
-                          <h3 className="h _5 color-grey-500">
-                            Video-based mocap systems
-                          </h3>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="display-flex">
-                <div className="width-33pct sizing-fixed border-bottom border-color-grey-600 m_width-45pct">
-                  <div className="padding-vertical padding-small">
-                    <div className="padding-right padding-small">
-                      <div className="display-flex gap-xx-small">
-                        <div>Real-time tracking</div>
-                        <div className="display-flex position-relative">
-                          <div className="info">i</div>
-                          <div className="info-tooltip">
-                            <h4 className="h font-weight-400 _5">
-                              Real-time tracking
-                            </h4>
-                            <p className="p small color-bright-100">
-                              The ability to view and manipulate the animation
-                              data as it is being captured
-                            </p>
-                            <div className="close color-bright-100 top-0 right-0 display-none t_display-flex">
-                              <div className="display-flex w-embed">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="14"
-                                  height="14"
-                                  fill="none"
-                                  stroke="currentColor"
-                                  strokeWidth="1.533"
-                                  strokeLinecap="round"
-                                >
-                                  <path d="M1.667 1.667l10.667 10.667"></path>
-                                  <path d="M12.333 1.667L1.667 12.333"></path>
-                                </svg>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="width-33pct sizing-fixed border-bottom border-color-grey-600 m_width-full">
-                  <div className="padding-vertical padding-small">
-                    <div className="padding-right padding-small">
-                      <div className="color-bright-100">Supported</div>
-                    </div>
-                  </div>
-                </div>
-                <div className="width-33pct sizing-fixed border-bottom border-color-grey-600 m_width-full">
-                  <div className="padding-vertical padding-small">
-                    <div className="padding-right padding-small">
-                      <div className="color-bright-100">Not supported</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="display-flex">
-                <div className="width-33pct sizing-fixed border-bottom border-color-grey-600 m_width-45pct">
-                  <div className="padding-vertical padding-small">
-                    <div className="padding-right padding-small">
-                      <div className="display-flex gap-xx-small">
-                        <div>Tracking space</div>
-                        <div className="display-flex position-relative">
-                          <div className="info">i</div>
-                          <div className="info-tooltip">
-                            <h4 className="h font-weight-400 _5">
-                              Tracking space
-                            </h4>
-                            <p className="p small color-bright-100">
-                              The area in which the motion capture system can be
-                              used
-                            </p>
-                            <div className="close color-bright-100 top-0 right-0 display-none t_display-flex">
-                              <div className="display-flex w-embed">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="14"
-                                  height="14"
-                                  fill="none"
-                                  stroke="currentColor"
-                                  strokeWidth="1.533"
-                                  strokeLinecap="round"
-                                >
-                                  <path d="M1.667 1.667l10.667 10.667"></path>
-                                  <path d="M12.333 1.667L1.667 12.333"></path>
-                                </svg>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="width-33pct sizing-fixed border-bottom border-color-grey-600 m_width-full">
-                  <div className="padding-vertical padding-small">
-                    <div className="padding-right padding-small">
-                      <div className="color-bright-100">
-                        Flexible, mocap anywhere
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="width-33pct sizing-fixed border-bottom border-color-grey-600 m_width-full">
-                  <div className="padding-vertical padding-small">
-                    <div className="padding-right padding-small">
-                      <div className="color-bright-100">
-                        Limited, distance constraints
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="display-flex">
-                <div className="width-33pct sizing-fixed border-bottom border-color-grey-600 m_width-45pct">
-                  <div className="padding-vertical padding-small">
-                    <div className="padding-right padding-small">
-                      <div className="display-flex gap-xx-small">
-                        <div>Set up time</div>
-                        <div className="display-flex position-relative">
-                          <div className="info">i</div>
-                          <div className="info-tooltip">
-                            <h4 className="h font-weight-400 _5">
-                              Set up time
-                            </h4>
-                            <p className="p small color-bright-100">
-                              Time required from idea to production-ready
-                              animation
-                            </p>
-                            <div className="close color-bright-100 top-0 right-0 display-none t_display-flex">
-                              <div className="display-flex w-embed">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="14"
-                                  height="14"
-                                  fill="none"
-                                  stroke="currentColor"
-                                  strokeWidth="1.533"
-                                  strokeLinecap="round"
-                                >
-                                  <path d="M1.667 1.667l10.667 10.667"></path>
-                                  <path d="M12.333 1.667L1.667 12.333"></path>
-                                </svg>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="width-33pct sizing-fixed border-bottom border-color-grey-600 m_width-full">
-                  <div className="padding-vertical padding-small">
-                    <div className="padding-right padding-small">
-                      <div className="color-bright-100">Fast, 1 min</div>
-                    </div>
-                  </div>
-                </div>
-                <div className="width-33pct sizing-fixed border-bottom border-color-grey-600 m_width-full">
-                  <div className="padding-vertical padding-small">
-                    <div className="padding-right padding-small">
-                      <div className="color-bright-100">Slow, 15 min</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="display-flex justify-center">
-            <a
-              href="/mocap/compare-mocap-suits-vs-video-based-mocap-systems"
-              className="button outline w-button"
-            >
-              See the full comparison
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
+        </Container>
+      </section>
+    </div>
   );
 };
 export default MocapSuits;
