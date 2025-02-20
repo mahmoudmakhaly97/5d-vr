68ea443df9dacf5db20e19cd7cceac2fcd9198e9
Mahmoud-123Makhaly <mahmoudmakhaly123@gmail.com>
2025-02-20 07:02:25 -0800

    --

--- src/pages/homepage/about-us/AboutUs.jsx
+++ src/pages/homepage/about-us/AboutUs.jsx
@@ -1,55 +1,51 @@
 import {   Container  } from "reactstrap";
  
-import img1 from "../../../assets/images/full-performance/Discover ai in xr 1.png";
-import img2 from "../../../assets/images/full-performance/Discover ai in xr 2.png";
-import img3 from "../../../assets/images/full-performance/Discover ai in xr 3.png";
-import img4 from "../../../assets/images/full-performance/Discover ai in xr 4.png";
-import img5 from "../../../assets/images/full-performance/Discover ai in xr 5.png";
-import img6 from "../../../assets/images/full-performance/Discover ai in xr 6.png";
-import img7 from "../../../assets/images/full-performance/Discover ai in xr 7.png";
-import img8 from "../../../assets/images/full-performance/Discover ai in xr 8.png";
-import img9 from "../../../assets/images/full-performance/Discover ai in xr 9.png";
 
-import img10 from "../../../assets/images/full-performance/Industries we serve 1.png"
-import img11 from "../../../assets/images/full-performance/Industries we serve 2.png"
-import img12 from "../../../assets/images/full-performance/Industries we serve 3.png"
-import img13 from "../../../assets/images/full-performance/Industries we serve 4.png"
-import img14 from "../../../assets/images/full-performance/Industries we serve 5.png"
-import img15 from "../../../assets/images/full-performance/Industries we serve 6.png"
-import img16 from "../../../assets/images/full-performance/Industries we serve 7.png"
-import img17 from "../../../assets/images/full-performance/Industries we serve 8.png"
-import img18 from "../../../assets/images/full-performance/Industries we serve 9.png"
-
-
-import img19 from "../../../assets/images/full-performance/Featured Projects 1.png"
-import img20 from "../../../assets/images/full-performance/Featured Projects 2.png"
-import img21 from "../../../assets/images/full-performance/Featured Projects 3.png"
-import img22 from "../../../assets/images/full-performance/Featured Projects 4.png"
-import img23 from "../../../assets/images/full-performance/Featured Projects 5.png"
-import img24 from "../../../assets/images/full-performance/Featured Projects 6.png"
-import img25 from "../../../assets/images/full-performance/Featured Projects 7.png"
-import img26 from "../../../assets/images/full-performance/Featured Projects 8.png"
-import img27 from "../../../assets/images/full-performance/Featured Projects 9.png"
 
  import HoveredCard from "./HoveredCard";
 const AboutUs = () => {
   const data = [
     {
-     images:[
-img1 ,img2 , img3,img4 , img5 , img6 , img7 , img8 , img9
-     ]
-     },
+      images: [
+        "/full-performance/Discover ai in xr 1.png",
+        "/full-performance/Discover ai in xr 2.png",
+        "/full-performance/Discover ai in xr 3.png",
+        "/full-performance/Discover ai in xr 4.png",
+        "/full-performance/Discover ai in xr 5.png",
+        "/full-performance/Discover ai in xr 6.png",
+        "/full-performance/Discover ai in xr 7.png",
+        "/full-performance/Discover ai in xr 8.png",
+        "/full-performance/Discover ai in xr 9.png"
+      ],
+    },
     {
-      images:[
-        img10 ,img11 , img12,img13 , img14 , img15 , img16, img17 , img18
-             ]
-     },
+      images: [
+        "/full-performance/Industries we serve 1.png",
+        "/full-performance/Industries we serve 2.png",
+        "/full-performance/Industries we serve 3.png",
+        "/full-performance/Industries we serve 4.png",
+        "/full-performance/Industries we serve 5.png",
+        "/full-performance/Industries we serve 6.png",
+        "/full-performance/Industries we serve 7.png",
+        "/full-performance/Industries we serve 8.png",
+        "/full-performance/Industries we serve 9.png"
+      ],
+    },
     {
-      images:[
-        img19 ,img20 , img21,img22 , img23 , img24 , img25 , img26 , img27
-             ]
-     },
+      images: [
+        "/full-performance/Featured Projects 1.png",
+        "/full-performance/Featured Projects 2.png",
+        "/full-performance/Featured Projects 3.png",
+        "/full-performance/Featured Projects 4.png",
+        "/full-performance/Featured Projects 5.png",
+        "/full-performance/Featured Projects 6.png",
+        "/full-performance/Featured Projects 7.png",
+        "/full-performance/Featured Projects 8.png",
+        "/full-performance/Featured Projects 9.png"
+      ],
+    },
   ];
+  
   return (
     <Container className="py-3 py-lg-5">
       <div >
